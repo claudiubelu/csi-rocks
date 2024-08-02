fc3749bfd328ba22d4cab0b0004cc151f625385d
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-08-02 14:27:39 +0300

    Adds csi-resizer rock (#7)

---
+++ tests/sanity/test_csi_resizer.py
@@ -0,0 +1,18 @@
+#
+# Copyright 2024 Canonical, Ltd.
+# See LICENSE file for licensing details
+#
+
+from k8s_test_harness.util import docker_util, env_util
+
+
+def test_csi_resizer_rock():
+    """Test csi-resizer rock."""
+    rock = env_util.get_build_meta_info_for_rock_version(
+        "csi-resizer", "1.10.1", "amd64"
+    )
+    image = rock.image
+
+    # check binary.
+    process = docker_util.run_in_docker(image, ["/csi-resizer", "--help"])
+    assert "Usage of /csi-resizer" in process.stderr
