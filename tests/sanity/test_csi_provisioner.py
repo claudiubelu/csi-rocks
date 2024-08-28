8c908a97dc40303806e19c7259d309066551ef46
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-08-28 11:17:26 +0000

    Adds csi-provisioner 4.0.1 rock
    Based on the 4.0.1 rock and upstream Dockerfile. Note that longhornio/csi-provisioner:v4.0.1
    is only a retag of the Kubernetes official image.
    
    Updates unit test to also test the new image.

--- tests/sanity/test_csi_provisioner.py
+++ tests/sanity/test_csi_provisioner.py
@@ -3,13 +3,15 @@
 # See LICENSE file for licensing details
 #
 
+import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 
-def test_csi_provisioner_rock():
+@pytest.mark.parametrize("image_version", ("4.0.0", "4.0.1"))
+def test_csi_provisioner_rock(image_version):
     """Test csi-provisioner rock."""
     rock = env_util.get_build_meta_info_for_rock_version(
-        "csi-provisioner", "4.0.0", "amd64"
+        "csi-provisioner", image_version, "amd64"
     )
     image = rock.image
 
