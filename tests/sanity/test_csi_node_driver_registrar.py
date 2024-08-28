5deb7483c9a614a285e8e36966ff21127db90e2d
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-08-28 11:21:16 +0000

    Adds csi-node-driver-registrar 2.11.1 rock
    Based on the 2.10.0 rock and upstream Dockerfile. The golang version was
    updated. Note that longhornio/csi-node-driver-registrar:v2.11.1 is only a
    retag of the Kubernetes official image.
    
    Updates unit test to also test the new image.

--- tests/sanity/test_csi_node_driver_registrar.py
+++ tests/sanity/test_csi_node_driver_registrar.py
@@ -3,13 +3,15 @@
 # See LICENSE file for licensing details
 #
 
+import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 
-def test_csi_node_driver_registrar_rock():
+@pytest.mark.parametrize("image_version", ("2.10.0", "2.11.1"))
+def test_csi_node_driver_registrar_rock(image_version):
     """Test csi-node-driver-registrar rock."""
     rock = env_util.get_build_meta_info_for_rock_version(
-        "csi-node-driver-registrar", "2.10.0", "amd64"
+        "csi-node-driver-registrar", image_version, "amd64"
     )
     image = rock.image
 
