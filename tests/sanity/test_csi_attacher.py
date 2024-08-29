ae8218c88ccaa26bbf9ac2621d2c79f49dfb190c
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-08-29 10:48:34 +0300

    Adds csi-attacher 4.6.1 rock (#14)
    Based on the 4.5.1 rock and upstream Dockerfile. The golang version was
    updated. Note that longhornio/csi-attacher:v4.6.1 is only a retag of
    the Kubernetes official image.
    
    Updates unit test to also test the new image.

--- tests/sanity/test_csi_attacher.py
+++ tests/sanity/test_csi_attacher.py
@@ -3,13 +3,15 @@
 # See LICENSE file for licensing details
 #
 
+import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 
-def test_csi_attacher_rock():
+@pytest.mark.parametrize("image_version", ("4.5.1", "4.6.1"))
+def test_csi_attacher_rock(image_version):
     """Test csi-attacher rock."""
     rock = env_util.get_build_meta_info_for_rock_version(
-        "csi-attacher", "4.5.1", "amd64"
+        "csi-attacher", image_version, "amd64"
     )
     image = rock.image
 
