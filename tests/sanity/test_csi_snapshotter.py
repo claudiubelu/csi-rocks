7ab5b702590d1fd29e900908dacec72547ed0357
Claudiu Belu <claudiu.belu@canonical.com>
2024-09-11 08:58:51 +0000

    Adds csi-snapshotter 6.3.4 rock
    Based on the 6.3.3 rock and upstream Dockerfile. Note that
    longhornio/csi-snapshotter:v6.3.4 is only a retag of the Kubernetes official image.
    
    Updates unit test to also test the new image.

--- tests/sanity/test_csi_snapshotter.py
+++ tests/sanity/test_csi_snapshotter.py
@@ -7,7 +7,7 @@ import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 
-@pytest.mark.parametrize("image_version", ("6.3.3", "7.0.2"))
+@pytest.mark.parametrize("image_version", ("6.3.3", "6.3.4", "7.0.2"))
 def test_csi_snapshotter_rock(image_version):
     """Test csi-snapshotter rock."""
     rock = env_util.get_build_meta_info_for_rock_version(
